33697c482a97ba5b3c7b474d992c0615df1667ea
George <greg@gregs-MacBook-Pro.local>
2019-10-29 23:07:10 -0700

    practice

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,27 @@
 $(document).ready(function() {
   $("form#leap-year").submit(function(event) {
     event.preventDefault();
-
+    // BUSINESS LOGIC
     var leapYear = function(year) {
-      return false;
+      if ((year % 100!==0) && (year % 4===0)) {
+        return true;
+      } else {
+        return false;
+      }
     };
-    
+    // USER INTERFACE LOGIC
     var year = parseInt($("input#year").val());
     var result = leapYear(year);
-    $("#result").text(result);
+    // $("#result").text(result);
+
+    $(".year").text(year);
+
+    if (!result) {
+      $(".not").text("not");
+      } else {
+        $(".not").text("");
+      }
+      $("#result").show();
+
   });
 });
